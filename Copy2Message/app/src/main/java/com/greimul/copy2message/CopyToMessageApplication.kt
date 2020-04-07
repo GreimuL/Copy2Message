bf3b88d5bfe56be30a37b112a44dfe36d1a133d3
greimul <yoseib1998@naver.com>
2020-04-07 11:06:24 +0900

    Add kakao api setting

---
+++ Copy2Message/app/src/main/java/com/greimul/copy2message/CopyToMessageApplication.kt
@@ -0,0 +1,18 @@
+package com.greimul.copy2message
+
+import android.app.Application
+import android.content.Context
+import com.kakao.auth.IApplicationConfig
+import com.kakao.auth.KakaoAdapter
+import com.kakao.auth.KakaoSDK
+
+class CopyToMessageApplication:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        KakaoSDK.init(object:KakaoAdapter(){
+            override fun getApplicationConfig(): IApplicationConfig {
+                return IApplicationConfig { this@CopyToMessageApplication }
+            }
+        })
+    }
+}
---
+++ Copy2Message/app/src/main/java/com/greimul/copy2message/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.greimul.copy2message
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kakao.auth.IApplicationConfig
+import com.kakao.auth.KakaoAdapter
+import com.kakao.auth.KakaoSDK
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
